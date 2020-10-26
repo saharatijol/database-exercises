f2e31ebf5fc8857fdc03ac23f83962e0d511b0d1
Sara T <sahara.tijol@gmail.com>
2020-10-26 13:54:18 -0500

    Exercise complete

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,10 +3,16 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS album;
 CREATE TABLE IF NOT EXISTS albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(25),
-    name VARCHAR(25),
+    artist VARCHAR(25) NOT NULL DEFAULT 'Unknown',
+    name VARCHAR(25) NOT NULL,
     release_date INT(4) UNSIGNED,
-    sales FLOAT(15),
-    genre VARCHAR(20),
+    sales FLOAT(11),
+    genre VARCHAR(20) DEFAULT 'Unknown',
     PRIMARY KEY(id)
-)
+);
+
+/*
+This sets field item to NOT allow Null entry and set "Unknown" as its default value instead
+Ex: author_last_name VARCHAR(25) NOT NULL DEFAULT 'Unknown'
+*/
+
