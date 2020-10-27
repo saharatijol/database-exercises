8d13b7d8ab90b8ccfde4cbca300fd22c68c158ce
Sara T <sahara.tijol@gmail.com>
2020-10-27 14:51:45 -0500

    Minor revisions to match examples with class materials

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,8 +3,8 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(25) NOT NULL DEFAULT 'Unknown',
-    name VARCHAR(25) NOT NULL,
+    artist VARCHAR(100) NOT NULL DEFAULT 'Unknown',
+    name VARCHAR(100) NOT NULL,
     release_date INT(4) UNSIGNED,
     sales FLOAT(11),
     genre VARCHAR(250) NOT NULL DEFAULT 'Unknown',
--- delete_exercises.sql
+++ delete_exercises.sql
@@ -5,7 +5,7 @@ DELETE FROM albums WHERE release_date > 1991;
 
 DELETE FROM albums WHERE genre = 'Disco';
 
-DELETE FROM albums WHERE artist = 'The Eagles';
+DELETE FROM albums WHERE artist = 'Eagles';
 
 -- #5 changed SELECT to DELETE
 
