c64099497f5ce8089514c0e40c2911f91bc68690
Sara T <sahara.tijol@gmail.com>
2020-10-27 11:35:11 -0500

    Added select_exercises.sql

---
+++ select_exercises.sql
@@ -0,0 +1,16 @@
+USE codeup_test_db;
+
+/**/
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT released_date FROM albums WHERE name = 'Supernatural';
+
+SELECT genre FROM albums WHERE name = 'Thriller';
+
+SELECT name FROM albums WHERE release_date BETWEEN 1989 AND 1999;
+
+SELECT name FROM albums WHERE sales < 20;
+
+SELECT * FROM albums;
+SELECT * FROM albums WHERE genre = 'Rock';
+
