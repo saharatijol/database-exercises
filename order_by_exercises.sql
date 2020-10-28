362836346b204a9ef44b5a82a7aa5bb7e9a99849
Sara T <sahara.tijol@gmail.com>
2020-10-28 11:22:32 -0500

    ORDER BY exercise completed

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,69 +1,47 @@
 USE employees;
 
--- #2
+-- ORDER BY #2
 SELECT *
 FROM employees
 WHERE first_name in ('Irena', 'Vidya', 'Maya')
 ORDER BY first_name;
 
--- #3
+-- ORDER BY #3
 SELECT *
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE first_name in ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
 
--- #4
+-- ORDER BY #4
 SELECT *
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+WHERE first_name in ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
 
--- #5
-SELECT *
-FROM employees
-WHERE birth_date like '%-12-25';
-
--- #6
-SELECT *
-FROM employees
-WHERE last_name like '%q%';
-
--- --------------------------------------
--- #1
-SELECT *
-FROM employees
-WHERE first_name = 'Irena'
-    OR first_name = 'Vidya'
-    OR first_name = 'Maya';
-
--- #2
-SELECT *
-FROM employees
-WHERE (first_name = 'Irena'
-   OR first_name = 'Vidya'
-   OR first_name = 'Maya')
-    AND gender = 'M';
-
--- #3
+-- ORDER BY #5
 SELECT *
 FROM employees
 WHERE last_name like 'E%'
-   OR last_name like '%E';
+   OR last_name like '%E'
+ORDER BY emp_no;
 
--- #4
+-- ORDER BY #6
+SELECT *
+FROM employees
+WHERE first_name in ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name DESC, first_name DESC;
+
 SELECT *
 FROM employees
 WHERE last_name like 'E%'
-  AND last_name like '%E';
+   OR last_name like '%E'
+ORDER BY emp_no DESC;
 
--- #5
+-- ORDER BY #7
 SELECT *
 FROM employees
 WHERE hire_date like '199%'
-   AND birth_date like '%-12-25';
-
--- #6
-SELECT *
-FROM employees
-WHERE last_name like '%q%'
-   AND last_name not like '%qu%';
+   AND birth_date like '%-12-25'
+ORDER BY birth_date, hire_date DESC;
 
 
