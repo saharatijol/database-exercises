2c1bb779ad1a8dffc1bdc56e6b40f65a3699e03a
Sara T <sahara.tijol@gmail.com>
2020-10-29 09:10:11 -0500

    Minor refactor after walkthrough

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,7 +3,7 @@ USE employees;
 -- #2
 SELECT *
 FROM employees
-WHERE first_name in ('Irena', 'Vidya', 'Maya');
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
 -- #3
 SELECT *
@@ -18,12 +18,12 @@ WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 -- #5
 SELECT *
 FROM employees
-WHERE birth_date like '%-12-25';
+WHERE birth_date LIKE '%-12-25';
 
 -- #6
 SELECT *
 FROM employees
-WHERE last_name like '%q%';
+WHERE last_name LIKE '%q%';
 
 -- --------------------------------------
 -- #1
@@ -44,25 +44,25 @@ WHERE (first_name = 'Irena'
 -- #3
 SELECT *
 FROM employees
-WHERE last_name like 'E%'
-   OR last_name like '%E';
+WHERE last_name LIKE 'E%'
+   OR last_name LIKE '%E';
 
 -- #4
 SELECT *
 FROM employees
-WHERE last_name like 'E%'
-  AND last_name like '%E';
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E';
 
 -- #5
 SELECT *
 FROM employees
-WHERE hire_date like '199%'
-   AND birth_date like '%-12-25';
+WHERE hire_date LIKE '199%'
+   AND birth_date LIKE '%-12-25';
 
 -- #6
 SELECT *
 FROM employees
-WHERE last_name like '%q%'
-   AND last_name not like '%qu%';
+WHERE last_name LIKE '%q%'
+   AND last_name NOT LIKE '%qu%';
 
 
