c70eed1863ec6ec88f4ed838530f8658095294a6
Sara T <sahara.tijol@gmail.com>
2020-10-29 15:38:21 -0500

    Consistent aliases

--- join_exercises.sql
+++ join_exercises.sql
@@ -10,7 +10,7 @@ WHERE dm.to_date = '9999-01-01'
 ORDER BY d.dept_name;
 
 -- #3.
-SELECT d.dept_name, CONCAT(e.first_name,' ', e.last_name) AS current_manager, e.gender
+SELECT d.dept_name, CONCAT(e.first_name,' ', e.last_name) AS current_manager
 FROM employees AS e
     JOIN dept_manager AS dm ON e.emp_no = dm.emp_no
     JOIN departments AS d ON dm.dept_no = d.dept_no
@@ -22,7 +22,7 @@ ORDER BY d.dept_name;
 
 
 -- #5
-SELECT d.dept_name, CONCAT(e.first_name,' ', e.last_name) AS Department_manager_name, s.salary AS Salary
+SELECT d.dept_name, CONCAT(e.first_name,' ', e.last_name) AS manager_name, s.salary AS manager_salary
 FROM employees AS e
         JOIN salaries AS s ON e.emp_no = s.emp_no
         JOIN dept_manager AS dm ON e.emp_no = dm.emp_no
@@ -31,13 +31,13 @@ WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
 ORDER BY d.dept_name;
 
 -- BONUS
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee Name', d.dept_name AS 'Department Name', CONCAT(e2.first_name, ' ', e2.last_name) AS 'Manager Name'
-FROM employees AS e
-         JOIN dept_emp AS de ON e.emp_no = de.emp_no
+SELECT CONCAT(e1.first_name, ' ', e1.last_name) AS employee_name, d.dept_name AS department_name, CONCAT(e2.first_name, ' ', e2.last_name) AS manager_name
+FROM employees AS e1
+         JOIN dept_emp AS de ON e1.emp_no = de.emp_no
          JOIN departments AS d ON d.dept_no = de.dept_no
          JOIN dept_manager AS dm ON d.dept_no = dm.dept_no
          JOIN employees AS e2 ON e2.emp_no = dm.emp_no
 WHERE de.to_date LIKE '9999%' and dm.to_date LIKE '9999%'
-ORDER BY e.last_name ASC;
+ORDER BY e1.last_name ASC;
 
 
