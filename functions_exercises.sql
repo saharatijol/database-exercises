c6e6a763fa4a94534a3de8945f7e2ae46f9cb296
Sara T <sahara.tijol@gmail.com>
2020-10-31 20:01:00 -0500

    mySQL extra practice

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -8,9 +8,9 @@ WHERE last_name LIKE 'E%'
 ORDER BY emp_no DESC;
 
 -- #3
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Name', hire_date, curdate(),
+SELECT CONCAT(first_name, ' ',last_name) AS 'Name', hire_date, curdate(),
        DATEDIFF(CURDATE(), hire_date) AS 'Days in company'
-FROM employees AS e
+FROM employees
 WHERE hire_date LIKE '199%'
   AND birth_date LIKE '%-12-25'
 ORDER BY birth_date, hire_date DESC;
