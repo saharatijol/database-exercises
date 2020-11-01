803dd90e8cd7a4f2db94dabe823e133244f478d9
Sara T <sahara.tijol@gmail.com>
2020-11-01 02:58:57 -0600

    String functions exercise complete

--- extra_practice/book_data.sql
+++ extra_practice/book_data.sql
@@ -92,3 +92,21 @@ SELECT CONCAT(author_lname, ' is ', CHAR_LENGTH(author_lname), ' characters long
 
 # UPPER() and LOWER() --only takes in 1 argument
 SELECT CONCAT('MY FAVORITE BOOK IS THE ', UPPER(title)) FROM books;
+
+# EXERCISES
+SELECT REVERSE(UPPER('Why does my cat look at me with such hatred?'));
+
+SELECT REPLACE(title, ' ', '->') AS title FROM books;
+
+SELECT author_lname AS forwards, REVERSE(author_lname) AS backwards FROM books;
+
+SELECT UPPER(CONCAT(author_fname, ' ', author_lname)) AS 'full name in caps' FROM books;
+
+SELECT CONCAT(title, ' was released in ', released_year) AS blurb FROM books;
+
+SELECT title, CHAR_LENGTH(title) AS character_count FROM books;
+
+SELECT CONCAT(SUBSTRING(title,1,10),'...') AS short_title,
+       CONCAT(author_lname, ',', author_fname) AS author,
+       CONCAT(stock_quantity, ' in stock') AS quantity
+FROM books;
