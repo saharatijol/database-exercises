4ab61b97622dea54e92f72ba55dd67803773617f
Sara T <sahara.tijol@gmail.com>
2020-11-02 00:16:29 -0600

    explored DATE_ADD and INTERVAL

--- extra_practice/dates.sql
+++ extra_practice/dates.sql
@@ -17,3 +17,27 @@ SELECT * FROM people;
 
 # FORMATTING DATES
 SELECT name, birthdate FROM people;
+SELECT name, birthdate, DAY(birthdate) FROM people;
+SELECT name, birthdate, DAYNAME(birthdate) FROM people;
+SELECT name, birthdate, DAYOFWEEK(birthdate) FROM people;
+SELECT name, birthdate, DAYOFYEAR(birthdate) FROM people;
+
+SELECT name, birth_dt, DAYOFYEAR(birth_dt) FROM people;
+SELECT name, birth_dt, MONTHNAME(birth_dt) FROM people;
+
+SELECT name, birth_dt, DATE_FORMAT(birth_dt, '%M %d %Y') FROM people;
+
+# DATE MATH
+SELECT name, birthdate, DATEDIFF(NOW(), birthdate) FROM people;
+SELECT  birth_dt, DATE_ADD(birth_dt, INTERVAL 1 MONTH) FROM people;
+SELECT  birth_dt, DATE_ADD(birth_dt, INTERVAL 1 MONTH) FROM people;
+SELECT birth_dt, DATE_ADD(birth_dt, INTERVAL 10 SECOND) FROM people;
+
+SELECT birth_dt, DATE_ADD(birth_dt, INTERVAL 3 QUARTER) FROM people;
+
+SELECT birth_dt, birth_dt + INTERVAL 1 MONTH FROM people;
+
+SELECT birth_dt, birth_dt - INTERVAL 5 MONTH FROM people;
+
+SELECT birth_dt, birth_dt + INTERVAL 15 MONTH + INTERVAL 10 HOUR FROM people;
+
