82721ffc4540c3c5d04b880278ab009b86029665
Sara T <sahara.tijol@gmail.com>
2020-11-04 01:17:11 -0600

    Finished case statements module

--- extra_practice/book_data4.sql
+++ extra_practice/book_data4.sql
@@ -90,3 +90,39 @@ SELECT title, released_year FROM books
 WHERE released_year >= 2000 AND released_year % 2 != 0
 ORDER BY released_year;
 
+# CASE STATEMENTS
+SELECT title, released_year,
+       CASE
+            WHEN released_year >= 2000 THEN 'Modern Lit'
+            ELSE '20th Century Lit'
+        END AS GENRE
+FROM books ORDER BY released_year;
+
+SELECT title, stock_quantity,
+       CASE
+            WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+            WHEN stock_quantity BETWEEN 51 AND 100 THEN '**'
+            ELSE '***'
+        END AS STOCK
+FROM books;
+
+SELECT title,
+       CASE
+           WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+           WHEN stock_quantity BETWEEN 51 AND 100 THEN '**'
+           WHEN stock_quantity BETWEEN 101 AND 150 THEN '***'
+           ELSE '****'
+        END AS STOCK
+FROM books;
+
+-- more succinct way
+SELECT title,
+       CASE
+           WHEN stock_quantity <= 50 THEN '*'
+           WHEN stock_quantity <= 100 THEN '**'
+           WHEN stock_quantity <= 150 THEN '***'
+           ELSE '****'
+        END AS STOCK
+FROM books;
+
+# LOGICAL OPERATORS EXERCISE
