49660767143146870884763651b5e26d476c1078
Sara T <sahara.tijol@gmail.com>
2020-11-03 23:24:09 -0600

    Revisit not like keyword

--- extra_practice/book_data4.sql
+++ extra_practice/book_data4.sql
@@ -11,4 +11,7 @@ WHERE released_year != 2017;
 SELECT title, author_lname FROM books
 WHERE author_lname != 'Harris';
 
-SELECT title FROM books WHERE title LIKE 'W%'
+SELECT title FROM books WHERE title LIKE '%W%';
+
+SELECT title FROM books WHERE title NOT LIKE '%W%';
+
