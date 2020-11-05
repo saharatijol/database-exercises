363be657ae8f9ee8940d1994794740f4b8ac984b
Sara T <sahara.tijol@gmail.com>
2020-11-05 00:23:00 -0600

    Practice explicit inner join

--- extra_practice/one-to-many.sql
+++ extra_practice/one-to-many.sql
@@ -55,3 +55,11 @@ WHERE customers.id = orders.customer_id;
 SELECT first_name, last_name, order_date, amount
 FROM customers, orders
 WHERE customers.id = orders.customer_id;
+
+-- EXPLICIT INNER JOIN = more conventional
+SELECT * FROM customers
+JOIN orders ON customers.id = orders.customer_id;
+
+SELECT first_name, last_name, order_date, amount
+FROM customers
+JOIN orders ON customers.id = orders.customer_id;
