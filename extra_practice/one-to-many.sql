d1a01b6a13717acadbdb2619166d71e3326a6eec
Sara T <sahara.tijol@gmail.com>
2020-11-07 15:22:00 -0600

    Last join exercise solved. Before walk thorugh

--- extra_practice/one-to-many.sql
+++ extra_practice/one-to-many.sql
@@ -165,7 +165,10 @@ ORDER BY average DESC;
 
 -- #5
 SELECT first_name, IFNULL(AVG(grade), 0) AS average,
-
+       CASE
+            WHEN IFNULL(AVG(grade), 0) >= 75 THEN 'PASSING'
+            ELSE 'FAILING'
+       END AS 'passing_status'
 FROM students
 LEFT JOIN papers p ON students.id = p.student_id
 GROUP BY first_name
