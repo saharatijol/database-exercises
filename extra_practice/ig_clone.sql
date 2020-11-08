44d716c35a18479390f28dd87d148ef73aa3757d
Sara T <sahara.tijol@gmail.com>
2020-11-07 22:10:54 -0600

    Built and tested likes schema

--- extra_practice/ig_clone.sql
+++ extra_practice/ig_clone.sql
@@ -7,22 +7,35 @@ CREATE TABLE users (
 );
 
 CREATE TABLE photos (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  image_url VARCHAR(255) NOT NULL,
-  user_id INT NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
-  FOREIGN KEY (user_id) REFERENCES users(id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE comments (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  comment_text VARCHAR(255) NOT NULL,
-  user_id INT NOT NULL,
-  photo_id INT NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
-  FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (photo_id) REFERENCES photos(id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    comment_text VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    photo_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (photo_id) REFERENCES photos(id)
 );
+
+-- NOTE: you can only like a corresponding photo only ONE time. We are not really storing any other information
+-- we are not adding id primary key here bec we are not going to be referring to likes, therefore don't need it
+CREATE TABLE likes (
+    user_id INT NOT NULL,
+    photo_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (photo_id) REFERENCES photos(id),
+    PRIMARY KEY(user_id, photo_id) -- this will not allow users to insert two likes that are exactly the same.
+        -- Gives you a unique combination
+);
+
 
 INSERT INTO users (username)
 VALUES('BlueTheCat'), ('CharlieBrown'), ('SaraTijol');
@@ -34,6 +47,20 @@ INSERT INTO comments(comment_text, user_id, photo_id)
 VALUES ('Funny picture.. lol', 1, 2),
        ('What happened?', 3, 2),
        ('Nice color', 2, 1);
+
+INSERT INTO likes (user_id, photo_id)
+VALUES (1,1),
+       (2,1),
+       (1,2),
+       (1,3),
+       (3,3);
+
+SELECT * FROM likes;
+
+-- This will NOT work bec of primary key constraint. Will not allow due to duplicate entry :)
+INSERT INTO likes (user_id, photo_id)
+VALUES
+(1,1);
 
 
 
