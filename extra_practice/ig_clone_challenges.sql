e651052ff0efd6e60235509d5c3a37f654827af7
Sara T <sahara.tijol@gmail.com>
2020-11-08 16:14:37 -0600

    Rerun challenge 4 with similar solution

--- extra_practice/ig_clone_challenges.sql
+++ extra_practice/ig_clone_challenges.sql
@@ -13,16 +13,24 @@ ORDER BY total DESC
 LIMIT 2;
 
 -- 3. Target inactive users with an email campaign. Find users who have never posted a photo
-SELECT username
+SELECT username, image_url
 FROM photos
 RIGHT JOIN users on photos.user_id = users.id
 WHERE image_url IS NULL;
 
--- 4. Identify most popular photo (and user who created it)
-SELECT username, photos.id, photos.image_url, COUNT(*) AS total
+-- Another solution
+SELECT username, photos.id
+FROM users
+LEFT JOIN photos ON users.id = photos.user_id
+WHERE photos.id IS NULL;
+
+-- 4. Who has the most likes on a single photo?
+SELECT username, photos.id, photos.image_url, COUNT(likes.user_id)
 FROM photos
-INNER JOIN likes ON likes.photo_id = photos.id
-INNER JOIN users ON photos.user_id = users.id
+JOIN likes ON likes.photo_id = photos.id
+JOIN users ON photos.user_id = users.id
 GROUP BY photos.id
-ORDER BY total DESC
+ORDER BY COUNT(likes.user_id) DESC
 LIMIT 1;
+
+
