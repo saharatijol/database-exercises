4df374b9a8a2d4ae41edb8b3c1a893a5c816512b
Sara T <sahara.tijol@gmail.com>
2020-11-07 21:40:38 -0600

    Start and tested photos schema

--- extra_practice/ig_clone.sql
+++ extra_practice/ig_clone.sql
@@ -6,7 +6,24 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT NOW()
 );
 
+CREATE TABLE photos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  image_url VARCHAR(255) NOT NULL,
+  user_id INT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
 INSERT INTO users (username)
 VALUES('BlueTheCat'), ('CharlieBrown'), ('SaraTijol');
 
+INSERT INTO photos(image_url, user_id)
+VALUES('/sdfsdfgs', 1), ('/ghjghj', 2), ('/vbnvbnvbnn', 2);
+
+
 SELECT * FROM users;
+SELECT * FROM photos;
+
+SELECT username, photos.image_url
+FROM photos
+JOIN users ON photos.user_id = users.id;
