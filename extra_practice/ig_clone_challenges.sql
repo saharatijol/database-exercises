89f36f94de1c2905a6c8519bed137695e2a67205
Sara T <sahara.tijol@gmail.com>
2020-11-08 16:47:32 -0600

    Find 5 most commonly used hashtags

--- extra_practice/ig_clone_challenges.sql
+++ extra_practice/ig_clone_challenges.sql
@@ -25,12 +25,27 @@ LEFT JOIN photos ON users.id = photos.user_id
 WHERE photos.id IS NULL;
 
 -- 4. Who has the most likes on a single photo?
-SELECT username, photos.id, photos.image_url, COUNT(likes.user_id)
+SELECT username, photos.id, photos.image_url, COUNT(*) AS total_likes
 FROM photos
 JOIN likes ON likes.photo_id = photos.id
 JOIN users ON photos.user_id = users.id
 GROUP BY photos.id
-ORDER BY COUNT(likes.user_id) DESC
+ORDER BY total_likes DESC
 LIMIT 1;
+
+
+-- 5. Our investors wan to know.. How many times does the average user post?
+-- Calculate avg number of photos per user
+-- total number of photos / total number of users
+SELECT
+    (SELECT COUNT(*) FROM photos) / (SELECT COUNT(*) FROM users) AS avg;
+
+-- 6. A brand wants to know which hashtags to use in a post. What are the top 5 most commonly used hashtags?
+SELECT tag_name, COUNT(*) AS 'total_tags_used'
+FROM photo_tags
+JOIN tags ON photo_tags.tag_id = tags.id
+GROUP BY tags.id
+ORDER BY total_tags_used DESC
+LIMIT 5;
 
 
