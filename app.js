34e31c14c2e0a8e4ee681b0d0404de609b51bf34
Sara T <sahara.tijol@gmail.com>
2020-11-08 20:45:56 -0600

    Selecting data and inserting data with nodeJS

--- app.js
+++ app.js
@@ -17,3 +17,29 @@ connection.query(q, function (error, results, fields) {
 });
 
 connection.end();
+
+
+// Another example: SELECTING DATA
+var q = 'SELECT * FROM users';
+
+connection.query(q, function (error, results, fields) {
+    if (error) throw error;
+    console.log(results[1].email);
+});
+connection.end();
+
+// INSERTING DATA
+// var q = 'INSERT INTO users(email) VALUES("rusty@gmail.com")';
+// connection.query(q, function (error, results, fields) {
+//     if (error) throw error;
+//     console.log(results);
+// });
+// connection.end();
+
+// INSERTING DATA PART 2
+var person = {email: faker.internet.email()};
+
+var end_result = connection.query('INSERT INTO users SET ?', person, function(err, result) {
+    if (err) throw err;
+    console.log(result);
+});
