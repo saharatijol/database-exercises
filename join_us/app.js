494dabd9e515aa0acb1350331abf4381f21be55a
Sara T <sahara.tijol@gmail.com>
2020-11-09 00:45:05 -0600

    Experiment with EJS templates

--- join_us/app.js
+++ join_us/app.js
@@ -1,16 +1,35 @@
 // To allow use of express.js
+let mysql = require('mysql');
 let express = require('express');
 let app = express();
 
+// Configure express application
+app.set("view engine", "ejs");
+
+// MySQL Database connection
+let connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    // Add password when in use later
+    database: 'join_us'
+});
+
 // Homepage Route
 app.get("/", function(req, res) {
-    console.log(req);
-    res.send("You've reached the homepage!")
+    // Find count of users in DB
+    let q = "SELECT COUNT(*) AS count FROM users";
+    connection.query(q, function(err, results) {
+       if (err) throw err;
+       let count = results[0].count;
+       res.send("We have " + count + " users in our db");
+    });
+    // Respond with that count
+    //res.send("We have " + count + " users in our db");
 });
 
 // Adding another Route
 app.get("/joke", function(req, res) {
-   let joke = "what do you call a dog that can do magic tricks? A labracadabrador.";
+   let joke = "<strong>what do you call a dog that can do magic tricks?</strong> A <em>labracadabrador.</em>";
     res.send(joke);
 });
 
@@ -20,6 +39,12 @@ app.get("/random_num", function(req, res) {
     res.send("Your luck number is " + num);
 })
 
+// This works here
 app.listen(8080, function () {
-    console.log("Server running on 8080!");
+    console.log("Server listening on port 8080!");
 });
+
+// This will work on goorm ide
+// app.listen(3000, function () {
+//     console.log("Server listening on port 3000!");
+// });
