56ea0dbc4d76cbedbb9038dfecb9da1f76c8eab8
Sara T <sahara.tijol@gmail.com>
2020-11-09 15:31:09 -0600

    Completed ig_clone tutorial

--- extra_practice/ig_trigger.sql
+++ extra_practice/ig_trigger.sql
@@ -1,9 +1,7 @@
 USE instagram_clone_db;
 
--- Let's prevent instagram self-follows with triggers
+# Let's prevent instagram self-follows with triggers
 -- NOT the best solution for actual application.
-
--- IF NEW.follower_id = NEW.followee_id
 
 DELIMITER $$
 
@@ -24,5 +22,51 @@ DELIMITER ;
 -- INSERT INTO follows(follower_id, followee_id) VALUES(5, 5);
 
 -- SELECT * from users;
+
+# Creating Logger Triggers - keep track of when somebody unfollows someone
+-- Basically, log your unfollows
+
+CREATE TABLE unfollows (
+       follower_id INT NOT NULL,
+       followee_id INT NOT NULL,
+       created_at TIMESTAMP DEFAULT NOW(),
+       FOREIGN KEY (follower_id) REFERENCES users(id),
+       FOREIGN KEY (followee_id) REFERENCES users(id),
+       PRIMARY KEY (follower_id, followee_id)
+);
+
+DELIMITER $$
+
+CREATE TRIGGER capture_unfollow
+    AFTER DELETE ON follows FOR EACH ROW
+    BEGIN
+        INSERT INTO unfollows
+        SET follower_id = OLD.follower_id,
+            followee_id = OLD.followee_id;
+    END;
+$$
+
+DELIMITER ;
+
+SELECT * FROM unfollows; -- currently empty
+SELECT * FROM follows LIMIT 5;
+
+DELETE FROM follows WHERE follower_id = 2 AND followee_id = 1;
+SELECT * FROM unfollows; -- only 1 in table
+
+DELETE FROM follows WHERE follower_id = 3; -- 99 rows affected
+SELECT * FROM unfollows;
+
+# LIST TRIGGERS
+SHOW TRIGGERS;
+
+# REMOVE TRIGGERS
+DROP TRIGGER capture_unfollow; -- I did not run this
+
+-- TIP: Triggers can make debugging hard! Avoid triggers as much as you can. Don't create too many of them
+
+
+
+
 
 
